6867623140832bca52205d7b3de029a421874be8
Raphaël Voyazopoulos <raphael.voyazopoulos@ircam.fr>
2018-05-28 17:22:12 +0200

    Creating client profile page

--- client/client/app/views.py
+++ client/client/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class ProfileView(TemplateView):
+
+    template_name = "profile.html"
